bae5039c1e13c905bb8c3bc5a6ea357a9da7ba1c
albert <albert@controlid.com.br>
2013-09-18 12:03:53 -0300

    Novo firmware para SAMD20 sem ASF

---
+++ FirmwareD20/leitorrf/Release/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+cmsis\src\startup_samd20.c
+
+cmsis\src\system_samd20.c
+
+leitorrf.c
+

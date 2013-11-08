06276c7b6aeac17fa2f9e2f78f70e8bce1d5aa3d
luiz <luizsol@controlid.com.br>
2013-11-08 16:57:09 -0200

    Primeiro commit do Luiz! Funciona o buzzer

--- FirmwareD20/leitorrf/leitorrf.c
+++ FirmwareD20/leitorrf/leitorrf.c
@@ -5,16 +5,30 @@
  *  Author: Albert
  */ 
 
-#include "sam.h"
+#include "sam.h"#include "component/component_tc.h"
 
-static inline void system_flash_set_waitstates(uint8_t wait_states)
+static inline void system_flash_set_waitstates(uint8_t wait_states) /* Para cada combinação de
+frequência do barramento de dados (AHB) e voltagem existe um wait state que otimiza o acesso à 
+memória não volátil (flash) (Pg 587). O wait state define como será feita a comunicação no 
+barramento de dados. */
 {
-	NVMCTRL->CTRLB.bit.RWS = wait_states;
+	NVMCTRL->CTRLB.bit.RWS = wait_states; /* NVMCTRL = "Non-Volatile Memory Controller" - Pg 264
+	(*NVMCTRL).
+	CTRLB.bit.RWS = Control B (Bits do registrador responsáveis por definir o wait state) (Pg 268)
+	Tudo indica que esse comando escreve no registrador certo o wait state da memória flash */
 }
 
-static inline int system_gclk_is_syncing(void)
+static inline int system_gclk_is_syncing(void)/*Essa função serve para verificar a flag de
+sincronização de registrador, ou seja, vai retornar 1 se está acontecendo sincronização e 0 se não
+está. Ela deve ser necessária para fazer com que outras funções que necessitem acessar 
+registradores que necessitam de sincronização não acabem tentando acessar o que não podem na hora
+errada, ou algo assim... */
 {
-	if (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY) {
+	if (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY) {/*GCLK = "Generic Clock" (Pg 78), serve para
+		receber diversos clocks externos e distribuir diferentes clocks internos onde for necessário
+		(sempre menores ou iguais aos da fonte). Pode ser encarado como um centro de distribuição
+		de clocks: recebe clocks em containers, daí os empacota no tamanho conveniente e manda pra
+		quem precisa. */
 		return 1;
 	}
 
@@ -25,6 +39,7 @@ void ClockInit()
 {
 	int i;
 	/* Workaround for errata 10558 */
+	/*SYSCTRL = System Controller (Pg 127) */
 	SYSCTRL->INTFLAG.reg = SYSCTRL_INTFLAG_BOD12RDY | SYSCTRL_INTFLAG_BOD33RDY |
 		SYSCTRL_INTFLAG_BOD12DET | SYSCTRL_INTFLAG_BOD33DET |
 		SYSCTRL_INTFLAG_DFLLRDY;
@@ -32,7 +47,7 @@ void ClockInit()
 	/* Configura os wait states de acesso à memória */
 	system_flash_set_waitstates(0);
 	
-	//Inicializar OSC8M
+	//Inicializar OSC8M (8MHz Internal Oscillator)
 	SYSCTRL->OSC8M.bit.PRESC = 0;
 	SYSCTRL->OSC8M.bit.ONDEMAND = 1;
 	SYSCTRL->OSC8M.bit.RUNSTDBY = 0;
@@ -100,7 +115,66 @@ void Init125khz(void)
 	TC2->COUNT16.CTRLA.bit.ENABLE = 1;
 }
 
+void initTC5(void){
+	//Associa o pino do Buzzer ao TC5
+	PORT->Group[0].PINCFG[25].bit.PMUXEN = 1;
+	PORT->Group[0].PMUX[12].bit.PMUXO = 0x5; //Periférico F = TC5/WO[1]
+
+	//Habilita o clock do TC5
+	PM->APBCMASK.reg |= PM_APBCMASK_TC5;
+	
+	//Reseta o TC5
+	TC5->COUNT16.CTRLA.bit.SWRST = 1;
+	while (TC5->COUNT16.CTRLA.bit.SWRST!=0);
+	
+	//Configura o TC5
+	TC5->COUNT16.CTRLA.reg = TC_CTRLA_WAVEGEN_MFRQ; //GCLK/1, não roda em STBY, MFRQ, COUNT16, Disable
+	TC5->COUNT16.CTRLBSET.reg = 0; //Count up infinitamente
+	TC5->COUNT16.CTRLC.reg = 0; //Sem capture/Compare e não inverte nenhuma saída
+	TC5->COUNT16.CC[0].reg = 1000; ////Valor de topo do counter (aqui que alteramos a freq!!!!)
+	
+	//Habilita o TC5
+	TC5->COUNT16.CTRLA.bit.ENABLE = 1;
+}
+
 volatile uint32_t a;
+
+void set_pino_off(uint8_t pino){
+	/** Configura e ativa um determinado pino **/
+	PORT->Group[0].PINCFG[pino].reg = 0;
+	PORT->Group[0].DIRSET.reg = (1 << pino);
+	PORT->Group[0].OUTCLR.reg = (1 << pino);
+}
+
+void set_pino_on(uint8_t pino){
+	/** Configura e ativa um determinado pino **/
+	PORT->Group[0].PINCFG[pino].reg = 0;
+	PORT->Group[0].DIRSET.reg = ( 1 << pino);
+	PORT->Group[0].OUTSET.reg = ( 1 << pino);
+}
+
+void delay(int tempo){
+	for(volatile int i = 0; i < tempo; i++){}
+}
+/*
+void dummy_buzz(int freq,int tempo){
+	volatile int buzzer = 0;
+	set_pino_off(4);
+	set_pino_on(3);
+	while (buzzer < tempo){
+		if(buzzer % 2){
+			set_pino_on(25);
+			delay(tempo/freq);
+			} else {
+			set_pino_off(25);
+			delay(tempo/freq);
+		}
+		buzzer++;
+	}
+	set_pino_off(3);
+	set_pino_on(4);
+	set_pino_off(25);
+}*/
 
 int main(void)
 {
@@ -110,7 +184,11 @@ int main(void)
 	/* Inicializa os clocks */
 	ClockInit();
 	Init125khz();
-
+	initTC5();
+	
+	//Habilita o clock do TC2 e do TC5
+	PM->APBBMASK.reg |= PM_APBBMASK_PORT;
+	
 	TC2->COUNT16.COUNT.reg = 22;
 	a=TC2->COUNT16.COUNT.reg;
 
